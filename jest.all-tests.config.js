ffe1f89d61343719774659a770e9f15c83caece2
htbkoo <h1017760@connect.hku.hk>
2018-08-10 23:04:20 +0800

    extracted configs from jest.all-tests.config.js to jest.config.js so that running test in IntelliJ would work by default freeCodeCamp - Apis and Microservices Projects - htbkoo/ExerciseTrackerMicroservice

--- jest.all-tests.config.js
+++ jest.all-tests.config.js
@@ -3,11 +3,4 @@ const baseConfig = require("./jest.config");
 module.exports = {
     ...baseConfig,
     displayName: "All tests",
-    testMatch: [
-        '**/test/**/*.(test|spec).(ts|js)'
-    ],
-    setupFiles: ["./test/setupTests.ts"],
-    globalTeardown: "./test/globalTeardown.js",
-    globalSetup: "./test/globalSetup.js",
-    testEnvironment: './test/mongo-environment.js',
 };
--- jest.config.js
+++ jest.config.js
@@ -12,4 +12,11 @@ module.exports = {
         '^.+\\.(ts|tsx)$': './node_modules/ts-jest/preprocessor.js'
     },
     testPathIgnorePatterns: ["/node_modules/", "/dist/.*"],
+    testMatch: [
+        '**/test/**/*.(test|spec).(ts|js)'
+    ],
+    setupFiles: ["./test/setupTests.ts"],
+    globalTeardown: "./test/globalTeardown.js",
+    globalSetup: "./test/globalSetup.js",
+    testEnvironment: './test/mongo-environment.js',
 };
