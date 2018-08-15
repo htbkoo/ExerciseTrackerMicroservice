aa024957b1d3d8342344959b1ad53b47bfceba62
htbkoo <h1017760@connect.hku.hk>
2018-08-16 07:35:39 +0800

    refactored (migrated from promise style to async/await) freeCodeCamp - Apis and Microservices Projects - htbkoo/ExerciseTrackerMicroservice

--- src/controllers/api/exercise.ts
+++ src/controllers/api/exercise.ts
@@ -14,7 +14,7 @@ const DATE_FORMAT = "yyyy-MM-dd";
 * Add exercise
 * */
 
-export let checkAddExerciseInputs = (req: Request): Promise<any> => new Promise(resolve => {
+export let checkAddExerciseInputs = async (req: Request): Promise<any> => new Promise(resolve => {
     req.check("userId", ValidationErrors.USERID_MISSING).exists();
     req.check("description", ValidationErrors.DESCRIPTION_MISSING).exists();
     req.check("duration", ValidationErrors.DURATION_NOT_NUMERIC).isNumeric();
@@ -22,20 +22,23 @@ export let checkAddExerciseInputs = (req: Request): Promise<any> => new Promise(
     resolve({req});
 });
 
-export let postAddExercise = (req: Request, res: Response, next: NextFunction) => {
+export let postAddExercise = async (req: Request, res: Response, next: NextFunction) => {
     const {userId, description, duration, date} = req.body;
     const docs = {userId, description, duration, date: firstDefined(date, todayInUtc())};
     const exercise = new Exercise(docs);
-    exercise.save()
-        .then(() => User.findOne({userId})) // TODO: to improve in case user not found
-        .then(user => {
-            logger.debug(`Corresponding user with id=${userId} is ${user.toString()}`);
-            res.send({
-                username: user.username,
-                ...docs
-            });
-        })
-        .catch(next);
+
+    try {
+        await exercise.save();
+        const user = await User.findOne({userId}); // TODO: to improve in case user not found
+
+        logger.debug(`Corresponding user with id=${userId} is ${user.toString()}`);
+        res.send({
+            username: user.username,
+            ...docs
+        });
+    } catch (e) {
+        next(e);
+    }
 };
 
 function checkDateFormat(req: Request, fieeld: string, acceptedDateFormat: string) {
