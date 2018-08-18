7d1accc0e1dbbdedba546c19fb73471dad4f13a6
htbkoo <h1017760@connect.hku.hk>
2018-08-18 12:13:49 +0800

    refactored freeCodeCamp - Apis and Microservices Projects - htbkoo/ExerciseTrackerMicroservice

--- src/controllers/common.ts
+++ src/controllers/common.ts
@@ -1,33 +1,28 @@
 "use strict";
 
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response, RequestHandler } from "express";
 import { DateTime } from "luxon";
 
 import logger from "../util/logger";
 
-export type Controller = (req: Request, res: Response, next: NextFunction) => void;
 export type ValidatorFunction = (req: Request, res: Response) => Promise<any>;
 
 /**
  * validation common middleware wrapper
  */
-export let validateFor = (fn: ValidatorFunction): Controller => {
-    return (req: Request, res: Response, next: NextFunction) => {
-        return fn(req, res)
-            .then(({req}) => {
-                    const errors = req.validationErrors();
-                    if (errors) {
-                        const e = JSON.stringify(errors);
-                        logger.debug(`validation error: ${e}`);
-                        req.flash("errors", errors);
-                        return next(e);
-                    }
-                    next();
+export let validateFor = (fn: ValidatorFunction): RequestHandler =>
+    (req: Request, res: Response, next: NextFunction): any =>
+        fn(req, res).then(({req}) => {
+                const errors = req.validationErrors();
+                if (errors) {
+                    const e = JSON.stringify(errors);
+                    logger.debug(`validation error: ${e}`);
+                    req.flash("errors", errors);
+                    return next(e);
                 }
-            );
-
-    };
-};
+                next();
+            }
+        );
 
 /**
  * Returns the value of param name when present,  in the following order: req.params, req.body, req.query
