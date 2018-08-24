1e016b6d29ffcb393b9cec9a312af7f96d326777
htbkoo <h1017760@connect.hku.hk>
2018-08-24 08:29:23 +0800

    fixed broken tests freeCodeCamp - Apis and Microservices Projects - htbkoo/ExerciseTrackerMicroservice

--- test/test-integration/controllers/home.spec.ts
+++ test/test-integration/controllers/home.spec.ts
@@ -4,8 +4,14 @@ import HttpStatus from "http-status";
 import app from "../../../src/app";
 
 describe("GET /", () => {
-    it("should return 200 OK", (done) => {
-        supertest(app).get("/")
-            .expect(HttpStatus.OK, done);
+    it("should have url query param populated if missing", () => {
+        return supertest(app).get("/")
+            .expect(HttpStatus.FOUND)
+            .expect("Location", "/?url=swagger/swagger.json");
+    });
+
+    it("should return 200 OK if url query param is already populated", () => {
+        supertest(app).get("/?url=someUrl")
+            .expect(HttpStatus.OK);
     });
 });
